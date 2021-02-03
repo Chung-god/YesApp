649c74570d2230da7e2791fe41015ade0ccbdc03
muzee99 <54929223+muzee99@users.noreply.github.com>
2021-02-03 22:58:55 +0900

    Using firebase + Vibrate

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,24 +7,51 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    @Override
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("is_crying");
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Vibrator v = (Vibrator)getSystemService(VIBRATOR_SERVICE);
 
-        Button btn = (Button)findViewById(R.id.btn);
+        Button btn = (Button)findViewById(R.id.btn1);
         btn.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View view){
-                Toast t = Toast.makeText(getApplicationContext(),"end",Toast.LENGTH_SHORT);
+                Toast t = Toast.makeText(getApplicationContext(),"Click Button",Toast.LENGTH_SHORT);
                 t.show();
-                Vibrator v = (Vibrator)getSystemService(VIBRATOR_SERVICE);
-                v.vibrate(1000);
+                v.cancel();
+                myRef.setValue(0);
+            }
+        });
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            private static final String TAG = "Error!";
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Long value = dataSnapshot.getValue(Long.class);
+                if(value == 1){
+                    Toast t = Toast.makeText(getApplicationContext(),"1",Toast.LENGTH_SHORT);
+                    t.show();
+                    v.vibrate(100000);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
     }
